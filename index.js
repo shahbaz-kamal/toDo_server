3e835c85f98df462e23f3db56a3dee1544abcd5f
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-02-21 20:12:23 +0600

    delete api written

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 const corsOptions = {
@@ -85,6 +85,13 @@ async function run() {
         ...newTask,
         createdAt: new Date(),
       });
+      res.send(result);
+    });
+
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
